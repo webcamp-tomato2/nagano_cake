9aa6dfa7cd07a77f965edc53cbfeaa57853161c0
yuri-misonoo <yuri.panpanda@gmail.com>
2021-08-16 08:11:14 +0000

    [add]ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get root to: 'homes#top'
+  get 'homes/about' => 'homes#about' 
   devise_for :admins, controllers: {
       sessions:      'admins/sessions',
       passwords:     'admins/passwords',
@@ -9,4 +11,29 @@ Rails.application.routes.draw do
       passwords:     'customers/passwords',
       registrations: 'customers/registrations'
   }
+  namespace :public do
+    resources :items, only: [:index, :show] do
+      resources :orders, only: [:show, :create]
+      post 'orders/comfirm' => 'orders#comfirm'
+    end
+    
+    resources :customers, only: [:show, :edit, :update] do
+      member do
+        get "check"
+        patch "withdrawl"
+      end
+      resources :shippings
+    end
+    
+    resources :cart_items, only: [:index, :create, :destroy]
+    resources :oder_items, only: [:index, :show]  
+  end
+  
+  namespace :admin do
+    resources :items
+    resources :customers, only: [:index, :show]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :oder_items, only: [:index, :show]
+  end
+    
 end
