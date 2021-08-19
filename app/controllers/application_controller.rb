67fa3ea2b07b7d69d6ee5b2a6f9ac2480e32804a
yuri-misonoo <yuri.panpanda@gmail.com>
2021-08-19 06:09:38 +0000

    [fix]管理者認証機能2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   #protect_from_forgery with: :exception
-  #def after_sign_in_path_for(resource)
-  #  case resource
-   # when Admin
-    #     admin_orders_path
-    #when Customer
-     #    homes_about_path
-    #end
-  #end
+  def after_sign_in_path_for(resource)
+  case resource
+    when Admin
+      admin_orders_path
+    when Customer
+      homes_about_path
+    end
+  end
 
   def after_sign_out_path_for(resource)
     case resource
