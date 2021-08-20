890a878e54fd55c43bc5eb514be9ed7cee2c5ce0
yuri-misonoo <yuri.panpanda@gmail.com>
2021-08-20 12:48:46 +0000

    [add]管理者：会員関連

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -2,18 +2,27 @@ class Admin::CustomersController < ApplicationController
   before_action :authenticate_admin!
 
   def index
+    @customers = Customer.all
   end
 
 
   def show
+    @customer = Customer.find(params[:id])
   end
   
   
   def edit
+    @customer = Customer.find(params[:id])
   end 
   
   
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer(@customer)
+    else
+      render :edit
+    end
   end 
   
 end
