62f5a98bf6e2dd9abdbb95dd7fc6ca8e07174378
Toyoda Yoshiteru <6089more@gmail.com>
2021-08-20 11:32:44 +0000

    [add]stash4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   #protect_from_forgery with: :exception
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
   def after_sign_in_path_for(resource)
     case resource
       when Admin
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,keys: [:first_name,:last_name,:first_name_kana,:last_name_kana,:portal_code,:address,:telephone_number])
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:first_name,:last_name,:first_name_kana,:last_name_kana,:postal_code,:address,:telephone_number])
   end
 
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,5 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-         
 
 end
