ca41dcf30d3cdbc64e244a217a39746b01548008
misamisa-create <missmis0329@gmail.com>
2021-08-22 03:22:23 +0000

    [add]合計金額

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,19 +3,27 @@ class Public::CartItemsController < ApplicationController
 
 def index
   @cart_items = current_customer.cart_items.all
-  # 調べる
-
+  # inject(0)で配列の合計を算出する
+  # 配列オブジェクト.inject(はじめの値){|初期値,要素|ブロック処理}
+  # @cart_itemsから１つずつitemを取り出してsumにitem.sum_of_priceを足す
+  @total = @cart_items.inject(0) { |sum, item| sum + item.sub_price }
 end
 
 
 def create
   @cart_item = CartItem.new(cart_item_params)
   @cart_item.customer_id = current_customer.id
+  # カートにある全ての情報を取得し
   @cart_items = current_customer.cart_items.all
+  # 数量を変更できるようにする記述
     @cart_items.each do |cart_item|
+      # すでにカートにあるitemと新しく追加したitemが同じ場合
       if cart_item.item_id==@cart_item.item_id
+        # 数量が追加されていく
         new_amount = cart_item.amount + @cart_item.amount
+        # モデル.update_attribute(属性値,値)で条件に一致するモデルオブジェクトを更新する
         cart_item.update_attribute(:amount, new_amount)
+        # この記述はわからん
         @cart_item.delete
       end
     end
@@ -39,6 +47,7 @@ end
 
 
 def all_destroy
+  # 全てを削除するのでfindではなくall
   @cart_items = current_customer.cart_items.all
   @cart_items.destroy_all
   redirect_to cart_items_path
