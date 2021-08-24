3f4fd060fea06582548ce75dfcca90ebf8f167bb
misamisa-create <missmis0329@gmail.com>
2021-08-24 07:55:36 +0000

    [add]

--- db/migrate/20210819063018_add_fisrt_name_to_customers.rb
+++
@@ -1,6 +0,0 @@
-class AddFisrtNameToCustomers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :customers, :first_name, :string
-    
-  end
-end
--- db/migrate/20210824070526_add_names_to_customers.rb
+++ db/migrate/20210824070526_add_names_to_customers.rb
@@ -1,8 +1,9 @@
-class AddLastNameToCustomers < ActiveRecord::Migration[5.0]
+class AddNamesToCustomers < ActiveRecord::Migration[5.2]
   def change
-    add_column :customers, :first_name_kana, :string
     add_column :customers, :last_name, :string
+    add_column :customers, :first_name, :string
     add_column :customers, :last_name_kana, :string
+    add_column :customers, :first_name_kana, :string
     add_column :customers, :postal_code, :string
     add_column :customers, :address, :string
     add_column :customers, :telephone_number, :string
