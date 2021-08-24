f918fb38dc2258a2cddcab4cbf3259173b2e8001
Toyoda Yoshiteru <6089more@gmail.com>
2021-08-24 05:57:34 +0000

    last_stage

--- db/migrate/20210823073752_add_order_id_to_order_detail.rb
+++
@@ -1,5 +0,0 @@
-class AddOrderIdToOrderDetail < ActiveRecord::Migration[5.2]
-  def change
-    add_column :order_details, :order_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_08_23_122302) do
+ActiveRecord::Schema.define(version: 2021_08_23_125607) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -86,8 +85,8 @@ ActiveRecord::Schema.define(version: 2021_08_23_122302) do
     t.integer "making_status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "order_id"
     t.integer "item_id"
+    t.integer "order_id"
   end
 
   create_table "orders", force: :cascade do |t|
