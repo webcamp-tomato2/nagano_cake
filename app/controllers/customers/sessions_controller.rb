f73673b57d8834129de710995f1468662bea8a1a
yuri-misonoo <yuri.panpanda@gmail.com>
2021-08-25 09:38:38 +0000

    [fix]退会機能

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -39,7 +39,7 @@ class Customers::SessionsController < Devise::SessionsController
     if @customer 
       if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == false)
         flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
-        redirect_to new_customer_registration
+        redirect_to new_customer_registration_path
       else
         flash[:notice] = "項目を入力してください"
       end
